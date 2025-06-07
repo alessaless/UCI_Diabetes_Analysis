7191325970c4ca6037dec1bcbbeff7c4f6dd95ca
PaoloCarmine1201 <paolocarmine1201@gmail.com>
2025-06-07 15:39:53 +0200

    feat: aggiunta analisi della skewness e generazione grafici per curtosi in colazione, pranzo e cena

--- Analisi/capitolo3/indiciDiSintesiFasceOrarie.r
+++ Analisi/capitolo3/indiciDiSintesiFasceOrarie.r
@@ -1,8 +1,35 @@
 library(dplyr)
 library(readr)
 library(tidyr)
+library(ggplot2)
 library(modeest)
+library(moments)
 
+
+# Funzione per plottare skewness e salvare in PDF
+plot_skewness <- function(dataframe, titolo_plot, nome_file_output) {
+  
+  # Calcola skewness
+  skew_val <- skewness(dataframe$Value)
+  
+  
+  # Genera il grafico
+  p <- ggplot(dataframe, aes(x = Value)) +
+    geom_histogram(aes(y = ..density..), bins = 30, fill = "#f47d0d", color = "white", alpha = 0.7) +
+    geom_density(color = "darkblue", size = 1) +
+    labs(
+      title = titolo_plot,
+      subtitle = paste0("Skewness = ", round(skew_val, 3)),
+      x = "Valori glicemici",
+      y = "Densità"
+    ) +
+    theme_minimal(base_size = 14)
+  
+  # Salva il grafico come PDF
+  ggsave(filename = paste0("./grafici_plot/", nome_file_output, ".pdf"),
+         plot = p, width = 8, height = 6)
+  
+}
 
 # STEP 1: Caricamento del dataset CSV
 data <- read_csv("./dataset/diabetes_clean_data3.csv", 
@@ -96,3 +123,38 @@ cv_cena <- (dev_std_cena / media_cena) * 100
 cat("📊 Coefficiente di Variazione Glicemia Colazione:", cv_colazione, "%\n")
 cat("📊 Coefficiente di Variazione Glicemia Pranzo:", cv_pranzo, "%\n")
 cat("📊 Coefficiente di Variazione Glicemia Cena:", cv_cena, "%\n")
+
+
+# Usa il dataset colazione
+plot_skewness(gliecemia_data_colazione, 
+              titolo_plot = "Distribuzione glicemia - Colazione", 
+              nome_file_output = "glicemia_skewness_Colazione")
+
+# Usa il dataset pranzo
+plot_skewness(gliecemia_data_pranzo, 
+              titolo_plot = "Distribuzione glicemia - Pranzo", 
+              nome_file_output = "glicemia_skewness_Pranzo")
+
+# Usa il dataset cena
+plot_skewness(gliecemia_data_cena, 
+              titolo_plot = "Distribuzione glicemia - Cena", 
+              nome_file_output = "glicemia_skewness_Cena")
+
+# Curtosi per colazione, pranzo e cena
+kurtosis_colazione <- kurtosis(gliecemia_data_colazione$Value)
+excess_kurtosis_colazione <- kurtosis_colazione - 3               # Curtosi in eccesso
+
+kurtosis_pranzo <- kurtosis(gliecemia_data_pranzo$Value)
+excess_kurtosis_pranzo <- kurtosis_pranzo - 3               # Curtosi in eccesso
+
+kurtosis_cena <- kurtosis(gliecemia_data_cena$Value)
+excess_kurtosis_cena <- kurtosis_cena - 3               # Curtosi in eccesso
+
+cat("📊 Curtosi Glicemia Colazione:\n")
+cat("Curtosi in eccesso:", round(excess_kurtosis_colazione, 3), "\n")
+
+cat("📊 Curtosi Glicemia Pranzo:\n")
+cat("Curtosi in eccesso:", round(excess_kurtosis_pranzo, 3), "\n")
+
+cat("📊 Curtosi Glicemia Cena:\n")
+cat("Curtosi in eccesso:", round(excess_kurtosis_cena, 3), "\n")
